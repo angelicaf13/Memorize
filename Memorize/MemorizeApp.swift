b4deee0420c0f75dfbaf316d96d9c8e7bf39aba9
Angelica Figueroa <angelica.figueroa@collins.com>
2024-10-30 14:58:10 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Angelica Figueroa on 30/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
